e0563a521ee6a235205488f05e41b4e235e1030c
Yusheng Li <tamamali@icloud.com>
2015-06-01 03:47:27 -0700

    add Bootstrap (theme: Lumen) support to master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,19 @@
-<html>
+<!DOCTYPE html>
+<html lang="zh-tw">
     <head>
+        <meta charset="utf-8">
+        <meta http-eqiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>App Name - @yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="stylesheets/bootstrap-theme.min.css">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+            <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+            <!--[if lt IE 9]>
+                <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+                <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <![endif]-->
     </head>
     <body>
         @section('sidebar')
@@ -10,5 +23,9 @@
         <div class="container">
             @yield('content')
         </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="javascripts/bootstrap.min.js"></script>
     </body>
 </html>
