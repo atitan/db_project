77e7dd2d9810f706caad8fa2ed252dadc6f69283
Yusheng Li <tamamali@icloud.com>
2015-06-10 16:08:55 +0800

    - add navbar, footer sections to the master lyout. - add some basic layout to the index page (Featured Products section remains unfinished) - add font-awesome. - add some img for members section. - add some custom css. - add Logo img.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,31 +1,171 @@
 <!DOCTYPE html>
 <html lang="zh-tw">
     <head>
-        <meta charset="utf-8">
-        <meta http-eqiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>App Name - @yield('title')</title>
-        <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="stylesheets/bootstrap-theme.min.css">
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-            <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-            <!--[if lt IE 9]>
-                <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-                <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            <![endif]-->
+      <meta charset="utf-8">
+      <meta http-eqiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>App Name - @yield('title')</title>
+      <link rel="stylesheet" type="text/css" href="../stylesheets/bootstrap.min.css">
+      <!-- Optional theme -->
+      <link rel="stylesheet" type="text/css" href="../stylesheets/bootstrap-theme.min.css">
+      <link rel="stylesheet" type="text/css" href="../font-awesome/css/font-awesome.min.css">
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <style>
+        body {
+          padding-top: 70px;
+        }
+        .img-logo {
+          display: inline-block;
+          height: 25px;
+        }
+        .navbar-inline>img {
+          display: inline-block;
+          margin-right: 10px;
+        }
+        @media screen and (max-width: 768px) {
+          .member-intro-content h3 {
+            text-align: center;
+          }
+
+        }
+        .member-intro {
+          margin: 10px 0;
+        }
+        .member-intro-content p {
+          text-indent: 2em;
+        }
+
+        /* Icon style */
+        .icon-sm{
+        	font-size:18px!important;
+        }
+        .icon-md{
+        	font-size:30px!important;
+        }
+        .icon-round{
+        	margin:0 auto;
+        	border:2px solid gray;
+        	border-radius: 50%;
+        	padding: 32px 30px 26px 30px;
+        	min-width:80px;
+        }
+        .icon-round,.icon-md,.icon-square.icon-md,.icon-rounded.icon-md{
+        	min-width: 92px;
+        }
+        .icon-round.icon-lg,.icon-square.icon-lg,.icon-rounded.icon-lg{
+        	min-width: 122px;
+        }
+        .icon-round.icon-xl,.icon-square.icon-xl,.icon-rounded.icon-xl{
+        	min-width: 162px;
+        }
+
+        /* Margin Style */
+        .v-margin-100 {
+          margin: 100px 0;
+        }
+        .v-margin-t0 {
+          margin-top: 0;
+        }
+        /* Hr Style */
+        /* Gradient transparent - color - transparent */
+
+        hr {
+          border: 0;
+          height: 1px;
+          background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
+        }
+        footer {
+          background: #424242;
+          color: #E0E0E0;
+          padding: 30px 0 20px 0;
+        }
+      </style>
     </head>
     <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
+      @section('navbar')
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand navbar-inline" href="#">
+                <img src="../img/Logo.png" class="img-responsive img-logo" alt="Brand" />購物商場
+              </a>
+            </div>
 
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <!-- <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+              </form> -->
+              <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#">首頁</a></li>
+                <li><a href="#">商品總覽</a></li>
+                <li><a href="#">購物車&nbsp;<span class="badge">3</span></a></li>
+                <li><a href="#">加入會員 / 登入</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    XX，您好！<span class="caret"></span>
+                  </a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">訂單查詢</a></li>
+                    <li><a href="#">會員資料更新</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">後台管理系統</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">登出</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      @show
+
+      @section('sidebar')
+      @show
+
+      <div class="container">
+          @yield('content')
+      </div>
+
+      <footer>
         <div class="container">
-            @yield('content')
+          <p>Copyright © Created by Sh Lin, Bihan Zhang, Yusheng Li at 2015. All Rights Reserved.</p>
+          <p>Theme: <a href="https://bootswatch.com/lumen/">Lumen</a> is made by <a href="http://thomaspark.co/">Thomas Park</a>. Based on <a href="http://bootstrap.com">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a>.</p>
         </div>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-            <!-- Include all compiled plugins (below), or include individual files as needed -->
-            <script src="javascripts/bootstrap.min.js"></script>
+      </footer>
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="javascripts/bootstrap.min.js"></script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,152 @@
 
 @section('title', 'Page Title')
 
-@section('sidebar')
-    @parent
-
-    <p>This is appended to the master sidebar.</p>
-@stop
-
 @section('content')
-    <p>This is index page.</p>
+<div class="row v-margin-100 v-margin-t0">
+  <div class="col-lg-8">
+    <!-- Carousel -->
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="img-responsive" src="http://www.placehold.it/1600x800" alt="...">
+          <div class="carousel-caption">
+            ...
+          </div>
+        </div>
+        <div class="item">
+          <img class="img-responsive" src="http://www.placehold.it/1600x800" alt="...">
+          <div class="carousel-caption">
+            ...
+          </div>
+        </div>
+        <div class="item">
+          <img class="img-responsive" src="http://www.placehold.it/1600x800" alt="...">
+          <div class="carousel-caption">
+            ...
+          </div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <!-- /. Carousel -->
+  </div>
+  <div class="col-lg-4">
+    <h2>Sample Title</h2>
+    <p>
+      必須先去了解市場和客戶的需求，一個一流的創意，生存下來的第一個想法是做好，什麼都想自己幹，我寧可喜歡一個一流的執行，結果還很遙遠，有結果未必是成功，別人可以拷貝我的模式，…愛情，退一步海闊天空，容易；生活，退一步海闊天空，天哪，沒有錢，再不對你好點，愛情，喝醉了我誰也不服,我就扶牆!我就像一隻趴在玻璃上的蒼蠅，住你的房，生，我希望有一天能用鼠標雙擊我的錢包，容易；生活，老闆，女人之美，生，年老的時候，不然不曉得奮鬥，愛情，男孩窮著養，問題是沒錢！
+    </p>
+  </div>
+</div>
+
+<hr />
+
+<div class="row v-margin-100">
+  <h2 class="text-center">Feature Product</h2>
+  <div class="col-lg-3">
+
+  </div>
+  <div class="col-lg-3">
+
+  </div>
+  <div class="col-lg-3">
+
+  </div>
+  <div class="col-lg-3">
+    
+  </div>
+</div>
+
+<hr />
+
+<div class="row v-margin-100">
+  <div class="col-sm-4">
+    <div class="text-center">
+      <span class="fa fa-heart icon-round icon-md"></span>
+    </div>
+    <h3 class="text-center mg-md">
+      Beautiful
+    </h3>
+    <p class="text-center">
+      A little feature description could go here. A little feature description could go here.
+    </p>
+  </div>
+  <div class="col-sm-4">
+    <div class="text-center">
+      <span class="fa fa-rocket icon-round icon-md"></span>
+    </div>
+    <h3 class="text-center mg-md">
+      Responsive
+    </h3>
+    <p class="text-center">
+      A little feature description could go here. A little feature description could go here.
+    </p>
+  </div>
+  <div class="col-sm-4">
+    <div class="text-center">
+      <span class="fa fa-code icon-round icon-md"></span>
+    </div>
+    <h3 class="text-center mg-md">
+      Clean
+    </h3>
+    <p class="text-center">
+      A little feature description could go here. A little feature description could go here.
+    </p>
+  </div>
+</div>
+
+<hr />
+
+<div class="row v-margin-100">
+  <div class="col-md-6 clearfix member-intro">
+    <div class="col-xs-offset-3 col-xs-6 col-sm-offset-0 col-sm-4">
+      <img src="img/members/Sh Lin.jpg" alt="" class="img-circle img-responsive"/>
+    </div>
+    <div class="col-xs-12 col-sm-8 member-intro-content">
+      <h3>Sh Lin &nbsp;<small>遙遠的車尾燈</small></h3>
+      <p>
+        大神一般的存在，因為速度神速，所以其他組員永遠都看不到車尾燈。最大的願望是死會。
+      </p>
+    </div>
+  </div>
+  <div class="col-md-6 clearfix member-intro">
+    <div class="col-xs-offset-3 col-xs-6 col-sm-offset-0 col-sm-4">
+      <img src="img/members/Bihan Zhang.jpg" alt="" class="img-circle img-responsive" />
+    </div>
+    <div class="col-xs-12 col-sm-8 member-intro-content">
+      <h3>Bihan Zhang &nbsp;<small>神隱的前端</small></h3>
+      <p>
+        神速成長的前端，因為討厭後端所以立志努力鑽研前端，發現 CSS3 是門玄學後，想回家種田、可爸爸又讓她成為三歲自耕農，只好繼續在前端的世界中浮沈。
+    </div>
+  </div>
+  <div class="col-md-6 clearfix member-intro">
+    <div class="col-xs-offset-3 col-xs-6 col-sm-offset-0 col-sm-4">
+      <img src="img/members/Yusheng Li.jpg" alt="" class="img-circle img-responsive" />
+    </div>
+    <div class="col-xs-12 col-sm-8 member-intro-content">
+      <h3>Yusheng Li &nbsp;<small>雜工小弱弱</small></h3>
+      <p>
+        前後端都只會一點點，因為弱得不像話所以常常被大神罵。最近外務眾多所以常常拖稿。
+      </p>
+    </div>
+  </div>
+</div>
+
+
 @stop
