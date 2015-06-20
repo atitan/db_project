5a8b6f0edbfa89b26930c553d1a6cfe9bc9c1828
Yusheng Li <tamamali@icloud.com>
2015-06-20 12:05:37 -0700

    remove misplaced code

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -30,39 +30,13 @@
           <input type="text" name="address" class="form-control" value="{{ $order->address }}" style="display: none;"></td>
 						<td class="text-right">{{ $order->total }}</td>
 						<td class="text-right">{{ $order->created_at }}</td>
-						<td><a href="/orders/{{ $order->id }}" style="text-decoration: none; margin-right: 8px;"><span><i class="fa fa-info-circle fa-fw"></i> 檢視訂單</span></a><a onclick="editOrder(this)" class="cursor" style="margin-right: 8px; text-decoration: none;"><span><i class="fa fa-pencil fa-fw"></i> 編輯</span></a>
+						<td><a href="/orders/{{ $order->id }}" style="text-decoration: none; margin-right: 8px;"><span><i class="fa fa-info-circle fa-fw"></i> 檢視訂單</span></a><a onclick="editOrder(this)" class="cursor" style="margin-right: 8px; text-decoration:none;"><span><i class="fa fa-pencil fa-fw"></i> 編輯</span></a>
           <a onclick="confirmEditOrder(this)" class="cursor"  style="margin-right: 8px; color: green; text-decoration: none; display: none;"><span><i class="fa fa-check fa-fw"></i> 確認</span></a>
           <a onclick="deleteOrder(this)" class="cursor" style="color: red; text-decoration: none;"><span><i class="fa fa-trash-o fa-fw"></i> 刪除</span></a></span></td>						
 					</tr>
 					@endforeach
 				</tbody>
 			</table>
-			<div id="orderDetails" class="panel-collapse collapse" role="tabpanel" aria-labelledby="orderDetails">
-			  <div class="panel-body">
-			    <table class="table table-hover table-responsive table-striped">
-			      <thead>
-			        <tr>
-			          <th style="max-width: 5em;">商品縮圖</th>
-			          <th>商品名稱</th>
-			          <th>商品單價</th>
-			          <th>選購數量</th>
-			          <th>小計</th>
-			        </tr>
-			      </thead>
-			      <tbody>
-			        @foreach($order as $order)
-			        <tr>
-			          <td style="max-width: 5em;"><img src="{{ $order->img_path }}" alt="{{ $order->name }}" class="img-responsive img-thumbnail"> </td>
-			          <td>{{ $order->name }}</td>
-			          <td class="text-right">{{ $order->price }}</td>
-			          <td class="text-right">{{ $order->quantity }}</td>
-			          <td class="text-right">{{ $order->extended_price }}</td>
-			        </tr>
-			        @endforeach
-			      </tbody>  
-			    </table>
-			  </div>
-			</div>
 		</div>
 	</div>
 @stop
